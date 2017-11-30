4605851e74cdc364216ba4c3456e2bfd438e6c5a
VarundeepSohal <33185868+VarundeepSohal@users.noreply.github.com>
2017-11-29 20:23:47 -0700

    Add files via upload
    settings.py file with MySQL database configuration.

--- Django/PVAZ_website/PVAZ_website/settings.py
+++ Django/PVAZ_website/PVAZ_website/settings.py
@@ -73,14 +73,23 @@ WSGI_APPLICATION = 'PVAZ_website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'pvaz',
+        'USER': 'django',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
